0033f7d0f15b3276f0022b365ef661f32d430692
Felipe Rosas <felipe.rosas@g.globo>
2022-09-06 20:00:10 -0300

    chore: add express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request, response) => {
+    response.send("API CEP");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
