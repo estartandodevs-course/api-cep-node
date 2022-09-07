1d8058aabcf1c0e225e9e64e19292a8608be9360
Felipe Rosas <felipe.rosas@g.globo>
2022-09-06 21:01:08 -0300

    fix: update db user variable

--- src/repositories/postgres/connection.js
+++ src/repositories/postgres/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = process.env.DB_PORT || 5432;
-const DB_USER = process.env.USER;
+const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
